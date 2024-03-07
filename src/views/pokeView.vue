53ffa6a537cdec3fdfbb86634eda193116e2e5a4
Rolando6297 <rolando.padilla@cceo.com.mx>
2024-03-07 10:50:55 -0600

    New feature (views/pokeView.vue): :sparkles: Upadate-poketView-to-show-message-when-pokemon-does-not-exist

--- src/views/pokeView.vue
+++ src/views/pokeView.vue
@@ -16,24 +16,28 @@ const getData = async () => {
         pokemon.value = data;
     } catch (error) {
         console.log(error);
+        pokemon.value = false;
     } finally {
         cargado.value = false;
     }
 }
 getData();
- 
+
 </script>
 
 <template>
-    <div v-if="cargado">
-        <div class="spinner-border" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-    </div>
+    <div class="text-center mt-5" v-if="!pokemon"><h1 class="text-danger text-center mt-5">No existe pokemon</h1> <button @click="back" class="btn btn-outline-success mt-4">Back</button></div>
     <div v-else>
-    <h1 >Poke name: {{ $route.params.pokemon }} </h1>
-    <p> Description {{pokemon.name }} </p>
-    <img :src="pokemon.sprites?.front_default" alt="cargando ...">
-    <button @click="back" class="btn btn-outline-success">Back</button>
+        <div v-if="cargado">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Cargando..</span>
+            </div>
+        </div>
+        <div v-else>
+            <h1>Poke name: {{ $route.params.pokemon }} </h1>
+            <p> Description {{ pokemon.name }} </p>
+            <img :src="pokemon.sprites?.front_default" alt="cargando ...">
+            <button @click="back" class="btn btn-outline-success">Back</button>
+        </div>
     </div>
 </template>
